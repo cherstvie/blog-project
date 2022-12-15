f782affe4fc529e15958675c109e9b3fed6a51ac
cherstvie <oxymanty@gmail.com>
2022-12-16 01:41:56 +0200

    start of developing blog-project

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,11 +1,5 @@
 import React from 'react'
-import { ButtonsField } from 'components/ButtonsField/ButtonsField'
 
 export const App = () => {
-    return (
-        <>
-            <h1>First task:</h1>
-            <ButtonsField />
-        </>
-    )
+    return <>Hello</>
 }
