19889af6b4fe89d0515ccc565c4b283d91009dab
cherstvie <oxymanty@gmail.com>
2022-12-17 21:34:24 +0200

    added App structure

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,14 @@
+import { Footer } from 'containers/Footer/Footer'
+import { Header } from 'containers/Header/Header'
+import { Main } from 'containers/Main/Main'
 import React from 'react'
 
 export const App = () => {
-    return <>Hello</>
+    return (
+        <>
+            <Header />
+            <Main />
+            <Footer />
+        </>
+    )
 }
